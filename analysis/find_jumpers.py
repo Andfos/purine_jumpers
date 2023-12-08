c48ace6435af0bd44c65a020c2aadbafd91de853
Andrew Foster <and.fos3995@gmail.com>
2023-12-08 17:11:59 +0100

    Added find_odd_even_homology.py

---
+++ analysis/find_jumpers.py
@@ -0,0 +1,145 @@
+import argparse
+
+
+# Parse CLI arguments.
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_file", required=True, help="Input file path")
+    parser.add_argument("-o", "--output_file", required=True, help='Output file path')
+    parser.add_argument(
+            "-w", "--window_size", default=200, 
+            help="Window size for nucelotides. Homology will be searched in the region <window_size> - <reference_size> (default: 200)")
+    parser.add_argument(
+            "-r", "--reference_size", default=40, 
+            help="Number of nucleotides to to use as reference for finding jumpers (default: 40)")
+    parser.add_argument(
+            "-m", "--min_homology", default=0.6, 
+            help="Minimum homology value to be considered a jumper (default : 0.6)")
+
+    args = parser.parse_args()
+    return args
+
+
+
+# Find the best matching string to a reference sequence.
+def find_best_matching_string(reference, sequence, min_homology=0.6):
+    
+    # Initialize variables.
+    best_homology = 0
+    best_match_string = ""
+    julen = 0
+    homology_dict = {}
+    
+    # Iterate along sequence.
+    for i in range(len(sequence) - len(reference) + 1):
+        substring = sequence[i:i + len(reference)]
+
+        # Count the number of matching characters and obtain homology.
+        match_count = sum(c1 == c2 for c1, c2 in zip(reference, substring))
+        homology = match_count / len(reference)
+        
+        # If the homology is greater than the best homology, record info.
+        if (homology >= best_homology):
+            best_homology = homology
+            jumper = substring
+            julen = i + len(reference)
+            
+            # Add information to a homology dictionary with keys as homology
+            # values.
+            if homology not in homology_dict:
+                homology_dict[homology] = []
+            temp_dict = {
+                    "homology" : homology,
+                    "jumper" : jumper,
+                    "julen" : julen}
+            homology_dict[homology].append(temp_dict)
+                                   
+    return homology_dict
+
+
+# Process the input file.
+def process_file(
+        input_file_path, ouput_file_path, 
+        window_size, reference_size, min_homology):
+    
+
+    with open(input_file_path, "r") as file, open(output_file_path, "w") as output_file:
+        
+        # Write the header of the output file.
+        output_file.write("position\thomology\tjulen\treference\tjumper\n")
+        
+        # Read the first 200 characters
+        window = file.read(window_size)
+        
+        position = 0
+        while len(window) == window_size:
+            nt = window[0]
+            
+            # Skip past windows that start with N
+            if "n" in window.lower():
+                print("yes")
+                position += 1
+                continue
+            
+            # Monitor progress by printing position every 100,000 nt.
+            if (position % 100000) == 0:
+                print(position)
+
+            # Take the first 40 characters as the reference
+            reference = window[:reference_size]
+
+            # Find the best matching string within the next 160 characters
+            homology_dict = find_best_matching_string(
+                    reference, window[reference_size:], min_homology)
+            
+            
+            
+            max_homo = max(homology_dict.keys())
+            if (len(homology_dict[max_homo]) > 1) or (max_homo <= min_homology):
+                position += 1
+                window = window[1:] + file.read(1)
+                continue
+
+            homology = homology_dict[max_homo][0]["homology"]
+            julen = homology_dict[max_homo][0]["julen"]
+            jumper = homology_dict[max_homo][0]["jumper"]
+
+            # Write results to file
+            output_file.write(
+                    f"{position}\t{homology}\t{julen}\t{reference}\t{jumper}\n"
+            )
+            
+            # Slide the window one character to the right
+            window = window[1:] + file.read(1)
+            position += 1
+
+
+
+# Main function to find jumpers.
+def main():
+
+    # Parse CLI arguments.
+    args = parse_arguments()
+    input_file = args.input_file
+    output_file = args.output_file
+    window_size = args.window_size
+    reference_size = args.reference_size
+    min_homology = args.min_homology
+    
+    print(input_file)
+    print(output_file)
+    print(window_size)
+    print(reference_size)
+    print(min_homology)
+    
+    raise
+    # Process the file to find jumpers.
+    process_file(
+            input_file, output_file, window_size, reference_size,
+            min_homology)
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ analysis/find_odd_even_homology.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+import argparse
+
+
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file")
+    parser.add_argument("output_file")
+    args = parser.parse_args()
+    return args
+
+
+
+def calculate_odd_even_homology(str1, str2):
+    """Calculate homology between odd and even characters in two strings.
+    
+    Parameters
+        ----------
+        str1 : str
+            The first input string.
+        str2 : str
+            The second input string.
+
+        Returns
+        -------
+        tuple
+            A tuple containing odd homology, even homology, relative difference
+            (Redif), and absolute difference (Abdif) between odd and even 
+            homology.
+        """
+
+    # Count the matching odd and even characters.
+    even_count = sum(c1 == c2 for c1, c2 in zip(str1[1::2], str2[1::2]))
+    odd_count = sum(c1 == c2 for c1, c2 in zip(str1[::2], str2[::2]))
+    
+    # Divide by the length of the strings to calculate homology.
+    even_homo = odd_count / len(str1[1::2])
+    odd_homo = even_count / len(str1[::2])
+    
+    # Get the relative difference between even homology and odd homology 
+    # Redif = (Eper - Oper), as well as the absolute difference ( abs(Redif) ).
+    redif = round(even_homo - odd_homo, 3)
+    abdif = abs(redif)
+
+    return odd_homo, even_homo, redif, abdif
+
+
+
+
+
+
+# Main function to get odd-even homology for jumpers.
+def main():
+    
+    # Parse CLI arguments.
+    args = parse_arguments()
+    input_file = args.input_file
+    output_file = args.output_file
+
+    # Read in the input_file to a dataframe.
+    df = pd.read_csv(input_file, sep="\t")
+
+
+    # Use np.frompyfunc to create a ufunc with multiple outputs
+    calculate_odd_even_homology_ufunc = np.frompyfunc(
+            calculate_odd_even_homology, 2, 4)
+
+    # Apply the ufunc to create new columns with the odd and even homology.
+    df[['odd_homology', 'even_homology', "redif", "abdif"]] = pd.DataFrame(
+        np.vstack(calculate_odd_even_homology_ufunc(df['reference'], df['jumper'])).T,
+        columns=['odd_homology', 'even_homology', "redif", "abdif"])
+
+    # Write the dataframe to file.
+    df.to_csv(output_file, sep="\t")
+
+
+
+if __name__ == "__main__":
+    main()
